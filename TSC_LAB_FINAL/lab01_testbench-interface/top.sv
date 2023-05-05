2d8a3f9539b317428080a4ea70827adb7b7a6817
Andreea Haldan <andreea.haldan@gmail.com>
2023-05-05 09:46:10 +0300

    Add Final Laborator

---
+++ TSC_LAB_FINAL/lab01_testbench-interface/top.sv
@@ -0,0 +1,42 @@
+/***********************************************************************
+ * A SystemVerilog top-level netlist to connect testbench to DUT
+ **********************************************************************/
+
+module top;
+ //DIRECTIVA DE COMPILATOR- CARE SETEAZA REZOLUTIA SI TIMPUL DE SIMULARE (AH)
+  timeunit 1ns/1ns;
+
+  // user-defined types are defined in instr_register_pkg.sv
+  import instr_register_pkg::*;
+
+  // clock variables
+  logic clk;
+  logic test_clk;
+
+  logic reset_n;
+
+  tb_ifc tb_ifc(clk, test_clk);
+
+  // instantiate testbench and connect ports
+  instr_register_test test (tb_ifc);
+
+  // instantiate design and connect ports
+  instr_register dut (tb_ifc);
+
+  // clock oscillators
+  initial begin
+    clk <= 0;
+    forever #5  clk = ~clk;
+  end
+
+  initial begin
+    test_clk <=0;
+    // offset test_clk edges from clk to prevent races between
+    // the testbench and the design
+    #4 forever begin
+      #2ns test_clk = 1'b1;
+      #8ns test_clk = 1'b0;
+    end
+  end
+
+endmodule: top
---
+++ TSC_LAB_FINAL/tools/regresion.bat
@@ -0,0 +1,7 @@
+call  run_test_wr_incr_rd_incr.bat c
+cd ../tools
+call  run_test_wr_incr_rd_random.bat c
+cd ../tools
+call  run_test_wr_random_rd_incr.bat c
+cd ../tools
+call  run_test_wr_random_rd_random.bat c
